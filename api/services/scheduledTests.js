1ac41be9abc335a8218afae7fe067fb1c8b5417c
Michal Kváček <michal@binarity.cz>
2016-05-05 02:25:14 +0200

    FEATURE: update scheduled tests

--- api/services/scheduledTests.js
+++ api/services/scheduledTests.js
@@ -1,19 +1,22 @@
 module.exports = {
 	run: function () {
-		var date = new Date (), test = {}, nextRun = {};
-		date.setSeconds (date.getSeconds + 60);
+		var test = {},
+			nextRun = {};
 
-		tests.findAll ({where: {nextRun: {$lt: date}}}).then (function (scheduledTests) {
-
+		tests.findAll ({where: {nextRun: {$lt: Date.now ()}}}).then (function (scheduledTests) {
 			for (i in scheduledTests) {
 				test = scheduledTests[i];
 
+				// add test to queue for running
+				// we need to run this task for each test, do not move it!
+				// this limit is because of selecting headers from environment and project
 				testRunner.addToQueue ({id: test.id}, console.error);
 
+				// and setup next time (or set NULL when should run only once)
 				nextRun = test.runInterval ? new Date (Date.now () + test.runInterval * 60 * 1000) : null;
 
 				test.update ({nextRun: nextRun}).then (function (updated) {
-					console.log('Scheduled test #'+test.id+' for '+updated.nextRun)
+					console.log ('Scheduled test #' + test.id + ' for ' + updated.nextRun)
 				});
 			}
 		})
--- config/cron.js
+++ config/cron.js
@@ -1,12 +1,20 @@
 module.exports.cron = {
+
+	/**
+	 * Add scheduled test into running queue
+	 */
 	runScheduledTests: {
 		schedule: '0 * * * * *',
 		onTick: function () {
-			// scheduledTests.run();
+			scheduledTests.run();
 		}
 	},
+
+	/**
+	 * Collect all finished requests/responses and mark test as failed or as passed
+	 */
 	markDoneTests: {
-		schedule: '*/10 * * * * *',
+		schedule: '*/25 * * * * *',
 		onTick: function () {
 			runnedTestsService.markLastRunStatus();
 		}
