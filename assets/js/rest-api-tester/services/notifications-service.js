bd587a37545e946da23238b6138e8eef53645941
Michal Kváček <michal@binarity.cz>
2016-05-06 15:56:03 +0200

    FEATURE: make notifications nicer

--- assets/js/rest-api-tester/services/notifications-service.js
+++ assets/js/rest-api-tester/services/notifications-service.js
@@ -3,12 +3,40 @@ var app = angular.module ('restApiTester');
 app.service ('notificationsService', ['$rootScope', function ($rootScope) {
 	$rootScope.globalNotifications = [];
 
+	/**
+	 * Delete all notifications
+	 */
 	$rootScope.hideAllNotifications = function () {
 		$rootScope.globalNotifications = [];
 	};
-	
+
+	/**
+	 * Delete only specified notification
+	 *
+	 * @param index index in array
+	 */
+	$rootScope.hideNotification = function (index) {
+		if ($rootScope.globalNotifications[index]) {
+			var notifications = $rootScope.globalNotifications;
+
+			// move all notifications to previous index
+			for (var i = index; i < notifications.length-1; i++) {
+				notifications[i] = notifications[i+1];
+			}
+			
+			// remove last item - is udplicated
+			$rootScope.globalNotifications = notifications.slice(0, notifications.length - 1);
+		}
+	};
+
 	var self = this;
 
+	/**
+	 * Display new notification
+	 *
+	 * @param type
+	 * @param content
+	 */
 	self.push = function (type, content) {
 		$rootScope.globalNotifications.push ({
 			time: Date.now (),
